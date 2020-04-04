e4986c8fd5e469205a383eba07cff3fd556cedc4
yusuke-hub <yk1210140408@gmail.com>
2020-04-04 11:38:39 +0900

    値を渡しながら画面遷移させる

--- Swift5Senni/Swift5Senni/ViewController.swift
+++ Swift5Senni/Swift5Senni/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var label: UILabel!
+    
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        count = count + 1
+        
+        label.text = String(count)
+        
+        if count == 10{
+            count = 0
+            performSegue(withIdentifier: "next", sender: nil)
+        }
+    }
+    
 }
 
