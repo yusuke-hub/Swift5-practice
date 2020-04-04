cb3fb03c8a322533ba4f33e89df40d86b15c631d
yusuke-hub <yk1210140408@gmail.com>
2020-04-04 11:15:00 +0900

    画面遷移アプリの作成 / 前の画面に戻るアクションを定義

--- Swift5KeyBoard1/Swift5KeyBoard1/ViewController.swift
+++ Swift5KeyBoard1/Swift5KeyBoard1/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//Appleがパーツに対するメソッドをProtocolとして準備してくれている
+//使うために、冒頭で宣言する
+class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet var logoImageView: UIImageView!
     
     @IBOutlet var userNameTextField: UITextField!
@@ -16,14 +18,28 @@ class ViewController: UIViewController {
 
     @IBOutlet var userNameLabel: UILabel!
     @IBOutlet var passwordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //delegateを自分のクラス(UIViewController)に設定
+        userNameTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
     @IBAction func login(_ sender: Any) {
+        logoImageView.image = UIImage(named: "loginOK")
         userNameLabel.text = userNameTextField.text
         passwordLabel.text = passwordTextField.text
+    }
+    
+    //タッチでキーボードを閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    //キーボードを閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
 }
---
+++ Swift5Senni/Swift5Senni/NextViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NextViewController.swift
+//  Swift5Senni
+//
+//  Created by 川端雄介 on 2020/04/04.
+//  Copyright © 2020 Yusuke Kawabata. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func back(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
