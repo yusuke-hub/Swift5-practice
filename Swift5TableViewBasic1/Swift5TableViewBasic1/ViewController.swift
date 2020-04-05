169f4f658c815dda794ac4277ee874ae866914f4
yusuke-hub <yk1210140408@gmail.com>
2020-04-05 10:41:17 +0900

    TableViewのdelegateメソッドを使用する準備

--- Swift5TableViewBasic1/Swift5TableViewBasic1/ViewController.swift
+++ Swift5TableViewBasic1/Swift5TableViewBasic1/ViewController.swift
@@ -8,16 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    
 
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
 
     }
-
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
 
 }
 
