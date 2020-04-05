30f5b44895e50d1c3be21325939286f266c760e7
yusuke-hub <yk1210140408@gmail.com>
2020-04-05 09:14:15 +0900

    WebViewアプリを作成

--- Swift5WKWebView1/Swift5WKWebView1/ViewController.swift
+++ Swift5WKWebView1/Swift5WKWebView1/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 // 元々はUIKitに含まれていた
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView = WKWebView()
     // 読み込みが完了するまで、くるくる回る
@@ -20,14 +20,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 大きさ
         webView.frame = CGRect(x: 0, y: toolBar.frame.size.height, width: view.frame.size.width, height: view.frame.size.width - toolBar.frame.size.height*2)
         view.addSubview(webView)
+        
+        // 何をロードするか
+        webView.navigationDelegate = self
+        let url = URL(string: "https://www.yahoo.co.jp/")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("読み込み開始")
+        indicator.startAnimating()
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("ロード完了")
+        indicator.stopAnimating()
     }
 
     @IBAction func go(_ sender: Any) {
+        webView.goForward()
     }
     
     @IBAction func back(_ sender: Any) {
+        webView.goBack()
     }
 }
 
