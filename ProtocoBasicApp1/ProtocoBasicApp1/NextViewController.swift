eee2ae632eb7e8bf3e4f41abecd67bf4e2b305d2
yusuke-hub <yk1210140408@gmail.com>
2020-04-05 17:42:01 +0900

    NextView, ラベルを宣言 / 変数(Int型)countに1を足していき、キャストして、ラベルのテキストに代入

--- ProtocoBasicApp1/ProtocoBasicApp1/NextViewController.swift
+++ ProtocoBasicApp1/ProtocoBasicApp1/NextViewController.swift
@@ -9,14 +9,23 @@
 import UIKit
 
 class NextViewController: UIViewController {
-
+    
+    
+    @IBOutlet var label: UILabel!
+    
+    var count:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func plusAction(_ sender: Any) {
+        count = count + 1
+        label.text = String(count)
+    }
+    
     /*
     // MARK: - Navigation
 
