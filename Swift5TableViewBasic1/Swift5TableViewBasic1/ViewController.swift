6007d4d3451b36524fbf1b419aa6750c97e1016b
yusuke-hub <yk1210140408@gmail.com>
2020-04-05 11:36:22 +0900

    TableViewのdelegateメソッドを定義 / TextFieldのdelegateメソッドを使用する準備

--- Swift5TableViewBasic1/Swift5TableViewBasic1/ViewController.swift
+++ Swift5TableViewBasic1/Swift5TableViewBasic1/ViewController.swift
@@ -19,13 +19,24 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        textField.delegate = self
 
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        returc cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.frame.size.height/6
     }
 
 }
