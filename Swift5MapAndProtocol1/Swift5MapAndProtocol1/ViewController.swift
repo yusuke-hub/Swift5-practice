f90ba4f616cd58cb768fcd46ac6e8bddc620cdbd
yusuke-hub <yk1210140408@gmail.com>
2020-04-06 10:21:09 +0900

    Delegateメソッド(CLLocationManagerDelegate,UIGestureRecognizerDelegate)の宣言 / IBActon(longPressTap)に条件分岐を追加 / viewDidLoad(settingButton)の設定

--- Swift5MapAndProtocol1/Swift5MapAndProtocol1/ViewController.swift
+++ Swift5MapAndProtocol1/Swift5MapAndProtocol1/ViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,CLLocationManagerDelegate,UIGestureRecognizerDelegate {
+    
+    var addressString = ""
+    @IBOutlet var longPress: UILongPressGestureRecognizer!
+    @IBOutlet var settingButton: UIButton!
+    @IBOutlet var mapView: MKMapView!
+    var locManager:CLLocationManager!
+    @IBOutlet var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        settingButton.backgroundColor = .white
+        // 角丸にする
+        settingButton.layer.cornerRadius = 20.0
     }
 
-
+    @IBAction func longPressTap(_ sender: UILongPressGestureRecognizer) {
+        // ここでのsenderはlongPressTap
+        if sender.state == .began{
+            
+        }else if sender.state == .ended{
+            
+        }
+    }
+    
 }
 
