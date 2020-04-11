72d66c7afc48ad427884de937b28ac8964ab4191
yusuke-hub <yk1210140408@gmail.com>
2020-04-11 10:42:47 +0900

    最初の画面、ボタンにタグを割り当てる

--- QuestionApp2/QuestionApp2/Controller/ViewController.swift
+++ QuestionApp2/QuestionApp2/Controller/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var maxScoreLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answer(_ sender: Any) {
+        
+        if (sender as AnyObject).tag == 1{
+            // ◯ボタンが押された時
+            
+            // ユーザーが押したボタンが○ボタンだった
+            
+            // ○ボタンの音声を流す
+        }else if (sender as AnyObject).tag == 2{
+            // ×ボタンが押された時
+            
+            // ユーザーが押したボタンが×だった
+            
+            // ×ボタンの音声を流す
+        }
+        
+    }
+    
+    
 }
 
