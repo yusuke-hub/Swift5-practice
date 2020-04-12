60d46bd8d4e7c73b68f432be75dd9381a9e1403c
yusuke-hub <yk1210140408@gmail.com>
2020-04-12 11:00:23 +0900

    カメラとアルバムの利用をチェックする

--- Swift5CameraAlbum1/Swift5CameraAlbum1/ViewController.swift
+++ Swift5CameraAlbum1/Swift5CameraAlbum1/ViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet var backImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        PHPhotoLibrary.requestAuthorization{(status) in
+            
+            switch(status){
+            case .authorized:
+                print("許可されています")
+
+            case .denied:
+                print("拒否されました")
+
+            case .notDetermined:
+                print("notDetermined")
+
+            case .restricted:
+                print("restricted")
+            }
     }
 
 
+    @IBAction func openCamera(_ sender: Any) {
+        
+        let sourceType = UIImagePickerController.SourceType.camera
+        // カメラが利用可能かどうかチェック
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            
+            // 変数化
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            cameraPicker.allowsEditing = true
+            present(cameraPicker, animated: true, completion: nil)
+                
+        }else{
+                print("エラー")
+        }
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func openAlbum(_ sender: Any) {
+        let sourceType = UIImagePickerController.SourceType.photoLibrary
+        // カメラが利用可能かどうかチェック
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            
+            // 変数化
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            cameraPicker.allowsEditing = true
+            present(cameraPicker, animated: true, completion: nil)
+                
+         }else{
+                print("エラー")
+         }
+                
+    }
 }
+
+
+    
+
 
