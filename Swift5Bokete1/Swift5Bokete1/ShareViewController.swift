0d69b16f1881cb2202ef5c77b70e67c3b5a0176a
yusuke-hub <yk1210140408@gmail.com>
2020-04-16 11:23:43 +0900

    画面遷移の設定 / 次の画面[パーツとプログラムを紐付け/スクリーンショットを撮る処理を記述

---
+++ Swift5Bokete1/Swift5Bokete1/ShareViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ShareViewController.swift
+//  Swift5Bokete1
+//
+//  Created by 川端雄介 on 2020/04/16.
+//  Copyright © 2020 Yusuke Kawabata. All rights reserved.
+//
+
+import UIKit
+
+class ShareViewController: UIViewController {
+    
+    var resultImage = UIImage()
+    var commentString = String()
+    
+    var screenShotImage = UIImage()
+
+    @IBOutlet var resultImageView: UIImageView!
+    
+    @IBOutlet var commentLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultImageView.image = resultImage
+        commentLabel.text = commentString
+        commentLabel.adjustsFontSizeToFitWidth = true
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func share(_ sender: Any) {
+        
+        // スクリーンショットを撮る
+        takeScreenShot()
+        // アクティビティ・ビューに乗っけて、シェアする
+        
+    }
+    
+    func takeScreenShot(){
+        
+        let width = CGFloat(UIScreen.main.bounds.size.width)
+        let height = CGFloat(UIScreen.main.bounds.size.height/1.3)
+        let size = CGSize(width: width, height: height)
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        
+        // viewに書き出す
+        self.view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+        screenShotImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Swift5Bokete1/Swift5Bokete1/ViewController.swift
+++ Swift5Bokete1/Swift5Bokete1/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
     
     @IBOutlet var searchTextField: UITextField!
     
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,16 +31,16 @@ class ViewController: UIViewController {
         PHPhotoLibrary.requestAuthorization{ (status) in
             
             switch(status){
-            case .authorized: break
-            case .denied: break
-            case .notDetermined: break
-            case .restricted: break
-         }
-     }
+                case .authorized: break
+                case .denied: break
+                case .notDetermined: break
+                case .restricted: break
+            }
+        }
         // Do any additional setup after loading the view.
     }
     
-    // 検索キーワードの値を序と日画像を引っ張ってくる
+    // 検索キーワードの値を元に画像を引っ張ってくる
     // pixabay.com
     
     func getImages(keyword:String){
@@ -46,14 +48,81 @@ class ViewController: UIViewController {
         // API Key
         let url = "https://pixabay.com/api/?key=16064611-59760cae7c854f0163b3ff527&q=\(keyword)"
         
-        // Alamofireを使ってhttpリクエストを投げる
-        
+        // Alamofire(ネットワーク通信の記述をシンプリ化する為のライブラリ)を使ってhttpリクエストを投げる
+        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+            
+            switch response.result{
+                
+            case .success:
+
+//                self.indicator.stopAnimating()
+                
+                let json:JSON = JSON(response.data as Any)
+                var imageString = json["hits"][self.count]["webformatURL"].string
+                if imageString == nil{
+                        
+                    imageString = json["hits"][0]["webformatURL"].string
+                    self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+                        
+                        
+                }else{
+                        
+                    self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+
+                }
+                    
+                    
+                    
+             case .failure(let error):
+                    
+                     print(error)
+                    
+                    
+             }
+                
+         }
         // 返ってきた値にJSON解析を行う
         
         // imageView.imageに貼り付ける
         
     }
 
-
+    @IBAction func nextOdai(_ sender: Any) {
+        
+        count = count + 1
+        
+        if searchTextField.text == ""{
+            
+            getImages(keyword: "funny")
+            
+        }else{
+            
+            getImages(keyword: searchTextField.text!)
+        }
+        
+    }
+    
+    @IBAction func searchAction(_ sender: Any) {
+        
+        self.count = 0
+        if searchTextField.text == ""{
+                
+                getImages(keyword: "funny")
+                
+        }else{
+                
+            getImages(keyword: searchTextField.text!)
+        }
+            
+    }
+    @IBAction func next(_ sender: Any) {
+        
+        performSegue(withIdentifier: "next", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let shareVC = segue.destination as? ShareViewController
+        shareVC?.commentString = commentTextView.text
+        shareVC?.resultImage = odaiImageView.image!
+    }
 }
 
