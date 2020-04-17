9663b865e7ae45f5348fbe5ec521c2b4b950d153
yusuke-hub <yk1210140408@gmail.com>
2020-04-17 17:24:11 +0900

    243 - 248

--- Swift5GoogleAdmob1/Swift5GoogleAdmob1/TableViewController.swift
+++ Swift5GoogleAdmob1/Swift5GoogleAdmob1/TableViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class TableViewController: UITableViewController {
-
+class TableViewController: UITableViewController,GADBannerViewDelegate,GADInterstitialDelegate {
+    
+    var profileImageArray = ["0","1","2","3","4"]
+    var textArray = ["顔文字0","顔文字1","顔文字2","顔文字3","インタースティシャル広告"]
+    
+    var interstitial:GADInterstitial!
+    
+    let backImageView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let image = UIImage(named: "backGroundImage")
+        backImageView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        backImageView.image = image
+        tableView.backgroundView = backImageView
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -24,14 +37,54 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        // bannerViewのセルの分を1足している
+        return 1 + textArray.count
     }
-
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 100
+        
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let indexNumber = indexPath.row
+        
+        if indexPath.row == 0{
+            
+            // インタースティシャル広告だけならば
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "BannerCell", for: indexPath)
+            let bannerView = cell.viewWithTag(1) as! GADBannerView
+            bannerView.adUnitID = "ca-app-pub-3940256099942544~1458002511"
+            bannerView.rootViewController = self
+            bannerView.load(GADRequest())
+            
+            return cell
+            
+        }else{
+            
+            // セルがあるならば
+            
+            let cell2 = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            // tagを1としたUIImageViewにテキストを表示する
+            let profileImageView = cell2.viewWithTag(1) as! UIImageView
+            // バナー用のセル１つ分をちゃんと引く
+            profileImageView.image = UIImage(named: profileImageArray[indexPath.row - 1])
+            // tagを2としたUILabelにテキストを表示する
+            let nameLabel = cell2.viewWithTag(2) as! UILabel
+            nameLabel.textColor = .white
+            nameLabel.font = .boldSystemFont(ofSize: 20)
+            nameLabel.numberOfLines = 3
+            nameLabel.text = textArray[indexNumber - 1]
+            
+            return cell2
+        }
+    }
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
--- Swift5GoogleAdmob1/Swift5GoogleAdmob1/ViewController.swift
+++ Swift5GoogleAdmob1/Swift5GoogleAdmob1/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bannerView.adUnitID = ""
+        bannerView.adUnitID = "ca-app-pub-3940256099942544~1458002511"
         bannerView.rootViewController = self
         // 広告を呼び出す
         bannerView.load(GADRequest())
