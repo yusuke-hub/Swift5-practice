be4c9cb876a75cf21e1d3894a40a45e063f363b8
yusuke-hub <yk1210140408@gmail.com>
2020-05-13 15:33:14 +0900

    347, 348

--- Swift5IntroApp1/Swift5IntroApp1/Controller/IntroViewController.swift
+++ Swift5IntroApp1/Swift5IntroApp1/Controller/IntroViewController.swift
@@ -7,18 +7,43 @@
 //
 
 import UIKit
-import Lo
+import Lottie
 
-class IntroViewController: UIViewController {
+class IntroViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet var scrollView: UIScrollView!
+    
+    // jsonファイルを入れる配列を準備
+    var onboardArray = ["1","2","3","4","5"]
+    var onboardStringArray = ["私たちはみんな繋がっています", "あいうえお","かきくけこ","さしすせそ","たちつてと"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // 1,2,3と止まる。ページングができるようになる
+        scrollView.isPagingEnabled = true
         
 
     }
     
-
+    func setUpScroll(){
+        scrollView.delegate = self
+        // 5ページ分の画面を表示できるようにする
+        scrollView.contentSize = CGSize(width: view.frame.size.width * 5, height: scrollView.frame.size.height)
+        
+        for i in 0...4{
+            let onboardLabel = UILabel(frame: CGRect(x: CGFloat(i) * view.frame.size.width, y: view.frame.size.height / 3, width: scrollView.frame.size.width, height: scrollView.frame.size.height))
+            onboardLabel.font = UIFont.boldSystemFont(ofSize: 15.0)
+            onboardLabel.textAlignment = .center
+            onboardLabel.text = onboardStringArray[i]
+            scrollView.addSubview(onboardLabel)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.isNavigationBarHidden = true
+    }
     /*
     // MARK: - Navigation
 
